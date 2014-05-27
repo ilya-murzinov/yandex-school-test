ac2685f6c478cea9fac0fd812ab82ed227adafdb
ilya-murzinov <murz42@gmail.com>
2014-05-27 23:54:22 +0400

    trying to solve problemB

--- src/ProblemB.java
+++ src/ProblemB.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class ProblemB {
@@ -10,19 +10,16 @@ public class ProblemB {
 
     public static void main(String[] args) {
         try {
-//            DataInputStream stream = new DataInputStream(System.in);
-//            InputStreamReader fileReader = new InputStreamReader(stream);
-//            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            InputStream inputStream = new FileInputStream(inName);
-            InputStreamReader fileReader = new InputStreamReader(inputStream);
+            //DataInputStream stream = new DataInputStream(System.in);
+            InputStream stream = new FileInputStream(inName);
+            InputStreamReader fileReader = new InputStreamReader(stream);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             numberOfWords = Integer.parseInt(bufferedReader.readLine());
             words = new String[numberOfWords];
             for (int i = 0; i < numberOfWords; i++) {
                 words[i] = bufferedReader.readLine();
             }
-            System.out.println(longestSubstring(new ArrayList<String>(Arrays.asList(words))));
+            System.out.println(longestSubstring(words));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,6 +33,43 @@ public class ProblemB {
         strings.remove(0);
         strings.set(0, substring);
         return longestSubstring(strings);
+    }
+
+    public static String longestSubstring(String[] strings) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int[] dimensions = new int[strings.length];
+        int length = 0;
+        for (int i = 0; i < strings.length; i++) {
+            dimensions[i] = strings[i].length();
+            if (dimensions[i] > length) {
+                length = dimensions[i];
+            }
+        }
+
+        String[] normalizedStrings = new String[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = 0; j < strings[i].length(); j++) {
+                normalizedStrings[i] += strings[i].charAt(j);
+            }
+            for (int j = strings[i].length(); j < length; j++) {
+                normalizedStrings[i] += "0";
+            }
+        }
+
+        Matrix matrix = new Matrix(dimensions);
+
+        for (int[] aMatrix : matrix) {
+            boolean equals = false;
+            for (int i = 0; i < aMatrix.length - 1; i++) {
+                if (i % aMatrix.length != 0){
+                    equals = equals &&
+                            (normalizedStrings[i].charAt(aMatrix[i]) ==
+                                    normalizedStrings[i+1].charAt(aMatrix[i+1]));
+                    System.out.println(equals);
+                }
+            }
+        }
+        return stringBuilder.toString();
     }
 
     public static String longestSubstring(String str1, String str2) {
@@ -66,8 +100,86 @@ public class ProblemB {
                         }
                     }
                 }
-            }}
+            }
+        }
 
         return sb.toString();
+    }
+}
+
+class Matrix implements Iterable<int[]> {
+    private List<Integer> list;
+    private int length;
+    private Iterator<int[]> iterator;
+
+    public Matrix(int[] dimensions) {
+        int length = 0;
+        for (int dimension : dimensions) {
+            if (dimension > length) {
+                length = dimension;
+            }
+        }
+        this.length = dimensions.length * length;
+        list = new ArrayList<Integer>();
+        for (int i = 0; i < this.length; i++) {
+            list.add(0);
+        }
+        iterator = new Index(dimensions.length, length);
+    }
+
+    public int get(int[] indexes) {
+        int resultingIndex = 0;
+
+        for (int i = 0; i < indexes.length; i++) {
+            resultingIndex += i + indexes[i];
+        }
+
+        return list.get(resultingIndex);
+    }
+
+    public int put(int[] indexes, int value) {
+        int resultingIndex = 0;
+
+        for (int i = 0; i < indexes.length; i++) {
+            resultingIndex += i + indexes[i];
+        }
+
+        return list.set(resultingIndex, value);
+    }
+
+    @Override
+    public Iterator<int[]> iterator() {
+        return iterator;
+    }
+}
+
+class Index implements Iterator<int[]> {
+    private int[] current;
+    private int length;
+
+    public Index(int n, int length) {
+        this.length = length;
+        current = new int[n];
+        for (int i = 0; i < current.length; i++) {
+            current[i] = 0;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return current[current.length - 1] < length;
+    }
+
+    @Override
+    public int[] next() {
+        int[] result = current;
+        for (int i = 0; i < current.length; i++) {
+            if (result[i] < length) {
+                result[i]++;
+                current = result;
+                return result;
+            }
+        }
+        return result;
     }
 }
